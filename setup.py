ed316e1b4dd7b69f9f52e87bbcf04d9711df0002
Michael Leopard <mleopard@gitlab.com>
2018-10-24 07:46:57 -0400

    Working on pipenv integration

--- setup.py
+++ setup.py
@@ -3,12 +3,20 @@ try:
 except:
     from distutils.core import setup
 
+from pipenv.project import Project
+from pipenv.utils import convert_deps_to_pip
+
+pfile = Project(chdir=False).parsed_pipfile
+requirements = convert_deps_to_pip(pfile['packages'], r=False)
+test_requirements = convert_deps_to_pip(pfile['dev-packages'], r=False)
+
 setup(
     name='t-gl',
     version='0.0.0',
     author="Brendan O'Leary",
     author_email='boleary@olearycrew.com',
     url='https://gitlab.com/brendan/t-gl',
+    install_requires=requirements,
     py_modules=['t'],
     entry_points={
         'console_scripts': [
